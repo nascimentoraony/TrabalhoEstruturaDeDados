131f4c2ae37e83d9ef9a5c25fd534212be60876b
Lucas Henrique <Lucas Henrique@192.168.137.170>
2019-03-10 19:54:21 -0300

    Implementação da classe ArquivoHandler.

--- trabalhoEstrutura/src/br/trabalhoEstrutura/ArquivoHandler.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/ArquivoHandler.java
@@ -1,5 +1,41 @@
 package br.trabalhoEstrutura;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ArquivoHandler {
 
+	public static void main(String[] args) {
+
+		try {
+
+			String nome = "";
+			String linha = "";
+			int cont = 0;
+			//Se der erro ao executar, referente ao tamanho do vetor colocar + 1
+			int tam_vetor = Integer.parseInt(linha); //+ 1
+
+			String vetor_txt[] = new String[tam_vetor];
+
+			FileReader arquivo = new FileReader(nome);
+			BufferedReader ler_arquivo = new BufferedReader(arquivo);
+
+			linha = ler_arquivo.readLine();
+
+			while (linha != null) {
+				System.out.println("\n" + linha);
+				linha = ler_arquivo.readLine();
+				cont++;
+				vetor_txt[cont] = linha;
+
+			}
+			arquivo.close();
+
+		} catch (IOException ex) {
+			System.err.printf("Erro: Não foi possível ler o arquivo!", ex.getMessage());
+
+		}
+
+	}
 }
