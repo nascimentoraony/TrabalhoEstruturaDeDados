ea76f0be3664c13e91950de3a7257153233eb07d
Lucas Henrique <Lucas Henrique@Samsung>
2019-03-11 23:50:29 -0300

    Alteração no cod, teste preenchimento vetor. Caminho alterado para leirura do arquivo.

--- trabalhoEstrutura/src/br/trabalhoEstrutura/ArquivoHandler.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/ArquivoHandler.java
@@ -1,42 +1,56 @@
 package br.trabalhoEstrutura;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ArquivoHandler {
 
 	public static void main(String[] args) {
 		
-		String nome = "";
+		String nome = "C:\\Users\\Lucas Henrique.LucasHenrique\\git\\Estrutura de Dados\\TrabalhoEstruturaDeDados\\trabalhoEstrutura\\src\\br\\trabalhoEstrutura\\teste.txt";
 		String linha = "";
-		//Se der erro ao executar, referente ao tamanho do vetor colocar + 1
-		int tam_vetor = Integer.parseInt(linha); //+ 1
-
-		try {
+		int tam = 0;
+		
+		try { 
 
 			int cont = 0;
 
-			String vetor_txt[] = new String[tam_vetor];
+			//String vetor_txt[] = new String[tam_vetor];
+			
 
 			FileReader arquivo = new FileReader(nome);
 			BufferedReader ler_arquivo = new BufferedReader(arquivo);
 
 			linha = ler_arquivo.readLine();
-
+			tam = Integer.parseInt(linha);
+			String vetor_txt[] = new String[tam];
 			while (linha != null) {
-				System.out.println("\n" + linha);
+				
+				
 				linha = ler_arquivo.readLine();
+				if(linha != null) {
+					vetor_txt[cont] = linha;
+					System.out.println(cont+" "+vetor_txt[cont]);
+				}
+				
 				cont++;
-				vetor_txt[cont] = linha;
 
 			}
 			arquivo.close();
+			
+			System.out.println("VETOR");
 
+			for(int i=0; i<tam; i++) {
+				System.out.println(vetor_txt[i]);
+			}
 		} catch (IOException ex) {
 			System.err.printf("Erro: Não foi possível ler o arquivo!", ex.getMessage());
 
 		}
+		
 
 	}
 }
