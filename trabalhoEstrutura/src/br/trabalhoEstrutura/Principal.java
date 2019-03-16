c7a30b06d92557005a9fe18d9bdbc76f4888355e
fabio <fabio@LAPTOP-7SGANU9O.mshome.net>
2019-03-15 22:05:56 -0300

    ok

--- trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
@@ -3,6 +3,9 @@ package br.trabalhoEstrutura;
 public class Principal {
 
 	public static void main(String[] args) {
+		Ordenacao ord = new Ordenacao();
+		String[] A = {"aaa","aa","aaaa","a","aaaaaaa","aaaaa","aaaaaa"};
+		ord.sort(A);
 		
 		
 		
--- trabalhoEstrutura/src/br/trabalhoEstrutura/SelectionSort.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/SelectionSort.java
@@ -4,18 +4,18 @@ public class SelectionSort {
 
 	public static void main(String[] args) {
 		
-		String[]  A = {"a","aaaaaaaaaaaaaa","aaaaaaaaaaaa","aaaaaaaaaaaaaaaaa","aaaaaaaaaaaaaaaaaaaaaaaaaaa",
-				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","aaaaaaaaa","aaaa","aaa"};
+		String[]  A = {"aaa","aa","aaaa","a","aaaaaaa","aaaaa","aaaaaa"};
 		int min = 0;
 		String atual;
-		
+		 
 		for (int i = 0;i<A.length;i++) {
-			for (int j = i+1;j<A.length-1;j++) {
-				if (A[i].length() > A[j].length()) {
+			min = i;
+			atual = A[i];
+			for (int j = i+1;j<A.length;j++) {
+				if (A[min].length() > A[j].length()) {
 					min = j; 
 				}
 			}
-			atual = A[i];
 			A[i] = A[min];
 			A[min] = atual;
 		}
