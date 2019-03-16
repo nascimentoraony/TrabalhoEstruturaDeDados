46dfb34aee20dd9fee454317c48b032f63757bbc
Raony Nascimento Nogueira <nascimento.raony@gmail.com>
2019-03-15 21:58:23 -0300

    principal Sta tic

--- trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
@@ -3,10 +3,8 @@ package br.trabalhoEstrutura;
 public class Principal {
 
 	public static void main(String[] args) {
-		Ordenacao ord = new Ordenacao();
 		String[] A = {"aaa","aa","aaaa","a","aaaaaaa","aaaaa","aaaaaa"};
-		ord.sort(A);
-		
+		Ordenacao.sort(A);
 		
 		
 	}
