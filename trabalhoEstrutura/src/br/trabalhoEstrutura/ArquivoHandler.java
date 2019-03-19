529f4b42e27c9542d6e1674720f2a5e18c27a952
Lucas.fDeus <Lucas.fDeus@Samsung>
2019-03-19 02:30:59 -0300

    Criação de arquivo (em branco)

--- trabalhoEstrutura/src/br/trabalhoEstrutura/ArquivoHandler.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/ArquivoHandler.java
@@ -4,56 +4,58 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class ArquivoHandler {
 
-	public static void main(String[] args) {
-		
-		String nome = "./arquivos/arquivo.txt";
-
+	public static void main(String[] args) throws IOException {
+		String nome = "./arquivos/portuguese.txt";
 		String linha = "";
 		int tam = 0;
 		
-		try { 
+		try {
 
 			int cont = 0;
-
-			//String vetor_txt[] = new String[tam_vetor];
-			
 
 			FileReader arquivo = new FileReader(nome);
 			BufferedReader ler_arquivo = new BufferedReader(arquivo);
 
 			linha = ler_arquivo.readLine();
-			tam =  Integer.parseInt(linha);
+			tam = Integer.parseInt(linha);
 			String vetor_txt[] = new String[tam];
+			
 			while (linha != null) {
-				
-				
+
 				linha = ler_arquivo.readLine();
-				if(linha != null) {
+				if (linha != null) {
 					vetor_txt[cont] = linha;
-					System.out.println(cont+" "+vetor_txt[cont]);
+					
 				}
-				
+
 				cont++;
 
 			}
 			arquivo.close();
-			
-			System.out.println("VETOR");
 
-			for(int i=0; i<tam; i++) {
+			for (int i = 0; i < tam; i++) {
 				System.out.println(vetor_txt[i]);
-				Ordenacao.sort(vetor_txt);
 			}
 			
+
 		} catch (IOException ex) {
 			System.err.printf("Erro: Não foi possível ler o arquivo!", ex.getMessage());
 
 		}
+		
+		File arquivo_ordenado = new File("./arquivos/ordenado.txt");
+		
+		try {
+		arquivo_ordenado.createNewFile();
+		}catch (IOException ex){
+			System.err.printf("Erro: aquivo não criado! ",ex.getMessage());
+			
+		}
+		
+		
 		
 	}
 }
