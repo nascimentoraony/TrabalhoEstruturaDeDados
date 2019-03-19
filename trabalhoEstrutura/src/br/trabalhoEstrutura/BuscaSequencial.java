81434a6d4e440c1f77786329210a551ba9c5b1f6
fabio <fabio@192.168.25.119>
2019-03-19 01:03:05 -0300

    Busca sequencial finalizada

--- trabalhoEstrutura/src/br/trabalhoEstrutura/BuscaSequencial.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/BuscaSequencial.java
@@ -1,5 +1,27 @@
 package br.trabalhoEstrutura;
 
 public class BuscaSequencial {
+		
+	public static void buscaSequencial(String A [], String search) {
+	        int i;
+	        int busca = 0;
+	        for(i = 0; i<10; i++)
+	        {
+	            if (search.equals(A[i]))
+	            {
+	                busca = 1;
+	                break;
+	            }
+
+	        }
+	        if (busca == 1)
+	        {
+	            System.out.println(search + ", palavra encontrada na " + (i+1) + " posição!" );
+	        }
+	        else
+	        {
+	            System.out.println("Palavra não encontrada !");
+	        }
+	}
 	
 }
--- trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
@@ -3,9 +3,10 @@ package br.trabalhoEstrutura;
 public class Principal {
 
 	public static void main(String[] args) {
-		String[] A = {"aaa","aa","aaaa","a","aaaaaaa","aaaaa","aaaaaa"};
-		Ordenacao.sort(A);
+		String[] A = {"aaa","aa","aaaa","a", "joao", "aaaaaaa","aaaaa","aaaaaa"};
+		//Ordenacao.sort(A);
 		
+		BuscaSequencial.buscaSequencial(A, "joao");
 	}
 
 }
