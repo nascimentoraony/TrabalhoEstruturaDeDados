eb0fa7278a59d83934a016c153edd5a02a7cb4b4
Lucas.fDeus <Lucas.fDeus@Samsung>
2019-03-19 02:41:24 -0300

    sth

--- trabalhoEstrutura/src/br/trabalhoEstrutura/ArquivoHandler.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/ArquivoHandler.java
@@ -4,60 +4,51 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArquivoHandler {
 
-	public static void main(String[] args) throws IOException {
-		String nome = "./arquivos/portuguese.txt";
+	public static void main(String[] args) {
+		
+		String nome = "./arquivos/arquivo.txt";
+
 		String linha = "";
 		int tam = 0;
 		
-		try {
+		try { 
 
 			int cont = 0;
+
+			//String vetor_txt[] = new String[tam_vetor];
+			
 
 			FileReader arquivo = new FileReader(nome);
 			BufferedReader ler_arquivo = new BufferedReader(arquivo);
 
 			linha = ler_arquivo.readLine();
-			tam = Integer.parseInt(linha);
+			tam =  Integer.parseInt(linha);
 			String vetor_txt[] = new String[tam];
-			
 			while (linha != null) {
-
+				
+				
 				linha = ler_arquivo.readLine();
-				if (linha != null) {
+				if(linha != null) {
 					vetor_txt[cont] = linha;
-					
+					System.out.println(cont+" "+vetor_txt[cont]);
 				}
-
+				
 				cont++;
 
 			}
 			arquivo.close();
-
-			for (int i = 0; i < tam; i++) {
-				System.out.println(vetor_txt[i]);
-			}
 			
+			System.out.println("VETOR");
 
 		} catch (IOException ex) {
 			System.err.printf("Erro: Não foi possível ler o arquivo!", ex.getMessage());
 
 		}
-		
-		//Criando arquivo em  branco.  
-		
-		File arquivo_ordenado = new File("./arquivos/ordenado.txt");
-		
-		try {
-		arquivo_ordenado.createNewFile();
-		}catch (IOException ex){
-			System.err.printf("Erro: aquivo não criado! ",ex.getMessage());
-			
-		}
-		
-		
 		
 	}
 }
--- trabalhoEstrutura/src/br/trabalhoEstrutura/Ordenacao.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/Ordenacao.java
@@ -3,28 +3,101 @@ package br.trabalhoEstrutura;
 import java.util.Arrays;
 
 public class Ordenacao {
+
 	
-	//modified
-	public static void main(String[] args) {
-	
-		String [] A = {"a", "aaaaaaa", "aaaaaaaaaaaaa", ""}; 
+	public static void SelectionSort (String A []){
+		int min = 0;
+		String atual;
+		 
+		for (int i = 0;i<A.length;i++) {
+			min = i;
+			atual = A[i];
+			for (int j = i+1;j<A.length;j++) {
+				if (A[min].length() > A[j].length()) {
+					min = j; 
+				}
+			}
+			A[i] = A[min];
+			A[min] = atual;
+		}
 		
-		quickSort(A, 0, A.length - 1);
+		for(int i = 0;i<A.length;i++) {
+			System.out.println(A[i]);
+		}
+	}
+	
+	public static void BubbleSort(String A []) {
+		boolean val;
+		do{
+			val = false;
+			for(int i = 0;i<(A.length-1);i++) {
+				if(A[i].length()>A[i+1].length()) {
+					String aux = A[i];
+					int a = A[i].length(); 
+					int b = A[i+1].length();
+					A[i] = A[i+1];
+					A[i+1] = aux;
+					val = true;
+				}
+			}
+		}while(val==true);
 		
 		System.out.println(Arrays.toString(A));
 	}
 	
-	public class QuickSort {
+	public static void MergeSort(String [] A) {
+
+		String ord [] = new String[A.length-1];
+				
+		System.out.println(Arrays.toString(A));
+		
+		dividir(A,0,A.length-1);
+		
+		System.out.println(Arrays.toString(A));
+				
+	}
+	
+	public static void dividir(String[] V,int min, int max) {
+		if(min<max) {
+			int med = (min+max)/2;
+			dividir(V, min, med);
+			dividir(V, med+1,max);
+			conquistar(V,min,med,max); 
+		}
+	}
+	
+	public static void conquistar(String[] V,int min, int med, int max) {
+		String aux[] = new String[V.length];
+		for(int i=min;i<=max;i++) {
+			aux[i] = V[i];
+		}
+		int i = min;
+		int j = med+1;
+		int k = min;
+		
+		while(i<=med && j<=max) {
+			if(aux[i].length()<=aux[j].length()) {
+				V[k] = aux[i];
+				i++;
+			} else {
+				V[k] = aux[j];
+				j++;
+			}
+			k++;
+		}
+		while(i<=med) {
+			V[k] = aux[i];
+			k++;
+			i++;
+		}
 		
 	}
 	
-	public static void sort(String [] A) {
-		quickSort(A, 0, A.length - 1);
-	}
-	
-	public static void quickSort(String A [], int begin, int end) {
+	public static void QuickSort(String A [], int begin, int end) {
 		int i = begin; 
-		int j = end; 
+		int j = end;
+		begin = 0;
+		end = A.length - 1;
 		String temp; 
 		String pivot = A[begin];
 		
@@ -49,10 +122,10 @@ public class Ordenacao {
 			}
 		}
 		if (begin < j) {
-			quickSort(A, begin, j);
+			QuickSort(A, begin, j);
 		}
 		else if (i < end) {
-			quickSort(A, i, end);
+			QuickSort(A, i, end);
 		}
 		System.out.println(Arrays.toString(A));
 		
