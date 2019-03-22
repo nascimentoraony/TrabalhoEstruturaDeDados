f3e975920d1d92ebf6c5fbb8531c7621bc342370
Raony Nascimento Nogueira <nascimento.raony@gmail.com>
2019-03-22 01:34:55 -0300

    a

--- trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
@@ -1,10 +1,12 @@
 package br.trabalhoEstrutura;
 
+import java.io.IOException;
+
 import br.teste.BuscaSequencial;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//Busca caminho do arquivo - seleção do usuário
 		//faz a leitura do tamanho do arquivo para criação do Vetor de movimentação da principal
 		String caminho = "./arquivos/portuguese.txt"; 
