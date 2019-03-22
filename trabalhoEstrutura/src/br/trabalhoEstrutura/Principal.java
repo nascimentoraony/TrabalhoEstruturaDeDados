76905c633bcb74514ff0170a312723c55a8b90a9
fabio <fabio@192.168.25.118>
2019-03-22 19:40:20 -0300

    Principal

--- trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
+++ trabalhoEstrutura/src/br/trabalhoEstrutura/Principal.java
@@ -15,6 +15,7 @@ public class Principal {
 		
 		// apenas comentado String caminho = "./arquivos/arquivo.txt"; 
 		
+		int op = 0;
 		String caminho = EscolherArquivo.caminho();
 		// criei uma classe no pacote de testes chamada de Escolher Arquivo possibilita a melhor inserção da
 		//mesma, pois não consegui ler ou gravar os arquivos, na classe principal.
@@ -27,7 +28,71 @@ public class Principal {
 		
 		JOptionPane.showMessageDialog(null, "<<<<<< BEM VINDO AO SISTEMA >>>>>");
 		
+	String [] ordenationOption = {"BubbleSort", "QuickSort", "MergeSort", "SelectionSort"};
+	int var = JOptionPane.showOptionDialog(null, "Escolha qual Método de Ordenação deseja executar;",
+            "OPÇÕES",
+            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, ordenationOption
+            , ordenationOption[0]);
+
+		do {
+			if (var == 0) {
+				long tempoInicial = System.currentTimeMillis();
+				
+				Ordenacao.BubbleSort(vetor);
+				
+				long tempoFinal = System.currentTimeMillis();
+				
+				JOptionPane.showMessageDialog(null, "BubbleSort Executado!");
+				
+				JOptionPane.showMessageDialog(null, "Executado em: " + (tempoFinal - tempoInicial) + "ms" );
+			}
+			
+			if (var == 1) {
+				long tempoInicial = System.currentTimeMillis();
+				
+				Ordenacao.QuickSort(vetor, 0, vetor.length);
+				
+				long tempoFinal = System.currentTimeMillis();
+				
+				JOptionPane.showMessageDialog(null, "QuickSort Executado!");
+				
+				JOptionPane.showMessageDialog(null, "Executado em: " + (tempoFinal - tempoInicial) + "ms" );
+			}
+			
+			if (var == 2) {
+				long tempoInicial = System.currentTimeMillis();
+				
+				Ordenacao.mergeSort(vetor, 0, vetor.length);
+				
+				long tempoFinal = System.currentTimeMillis();
+				
+				JOptionPane.showMessageDialog(null, "MergeSort Executado!");
+				
+				JOptionPane.showMessageDialog(null, "Executado em: " + (tempoFinal - tempoInicial) + "ms" );
+			}
+			
+			if (var == 3) {
+				long tempoInicial = System.currentTimeMillis();
+				
+				Ordenacao.SelectionSort(vetor);
+				
+				long tempoFinal = System.currentTimeMillis();
+				
+				JOptionPane.showMessageDialog(null, "SelectionSort Executado!");
+				
+				JOptionPane.showMessageDialog(null, "Executado em: " + (tempoFinal - tempoInicial) + "ms" );
+			}
+			
+			op = JOptionPane.showConfirmDialog(null, "Deseja alterar o método de Ordenação?");
+		} while (op == 0);
+		
+		
+		
+		
 		System.out.println("lido");
+		
+		
+		
 		
 		//Ordenacao.QuickSort(vetor, 0, vetor.length - 1);
 		
